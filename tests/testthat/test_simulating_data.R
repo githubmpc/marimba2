6b4563b6f3b3d6548bbf9cf9a1e5096bc0470a29
githubmpc <mchou5@jhmi.edu>
2018-04-09 14:45:32 -0400

    unit test for simulation

--- tests/testthat/test_simulating_data.R
+++ tests/testthat/test_simulating_data.R
@@ -1,40 +1,61 @@
 context("Simulating data")
 
-test_that("simulate_data", {
-  set.seed(668899)
+test_that("simulate_data_multi", {
+  set.seed(98765)
   ##mendelian.probs <- mendelianProb(epsilon=0)
-  mendelian.probs <- gMendelian(tau.one=1, tau.two=0.5, tau.three=0, err=0)
-  path <- system.file("extdata", package="marimba")
-  vcf.del.df.4 <- read.table(file.path(path, "parameter.p.1000gp.v2.txt"),
-                             header=TRUE, sep="\t")
-  median.sd.df.4 <- read.table(file.path(path, "medians.sd.txt"),
-                               header=TRUE, sep="\t")
-  i <- 108 ## region 108 
-  p.1000gp <- vcf.del.df.4[i, c(9:11)]
-  p.1000gp <- as.numeric(p.1000gp)
-  theta.1000gp <- median.sd.df.4[i, c(1:3)]
-  theta.1000gp <- as.numeric(theta.1000gp)
-  sigma.1000gp <- median.sd.df.4[i, c(4:6)]
-  sigma.1000gp <- as.numeric(sigma.1000gp)
-  ## custom input as required (optional)
-  ##sigma.1000gp[1] <- .08
-  ##p.1000gp <- c(.1, .2, .7)
-  ##dat <- simulate_data(81, p = p.1000gp,
-  ##                     theta = theta.1000gp,
-  ##                     sigma = sigma.1000gp,
-  ##                     mendelian.probs)
-  params <- statistics_1000g(region=108)
+  p <- c(0.09, 0.24, 0.34, 0.24, 0.09)
+  theta <- c(-3.5,-1.2, 0.3, 1.7, 4)
+  sigma <- c(0.2, 0.2, 0.2, 0.2, 0.2)
+  params <- data.frame(cbind(p, theta, sigma))
+  gp<-geneticParams(K=5, states=0:4, xi=c(0.2, 0.2, 0.2, 0.2, 0.2), 
+                    mu=c(-3.5, -1.2, 0.3, 1.7, 4))
+  #mendelian.probs <- gMendelian.multi()
   expect_equal(sum(params[, "p"]), 1)
-  #params[, "p"] <- c(0.1, 0.2, 0.7)
-  #params[1, "sigma"] <- 0.08
-  set.seed(668899)
-  dat2 <- simulate_data2(params, N=81, error=0)
-  ##y1 <- dat$response
-  ##y2 <- dat2$y
-  ##colnames(y1) <- colnames(y2) <- NULL
-  ##expect_identical(y1, y2)
-  if(FALSE) {
-    saveRDS(dat2, "../../inst/extdata/simulation_easy.rds")
-    gg_cnp(dat2)
-  }
+  
+  dat2 <- simulate_data_multi(params, N=500, error=0, gp)
+  expect_that(length(levels(factor(dat2$data$copy_number))) == length(theta), is_true())
+  expect_equal(p, dat2$params$p, tolerance=0.05)
+  expect_equal(theta, dat2$params$theta, tolerance=0.03)
+  expect_equal(sigma, dat2$params$sigma, tolerance=0.03)
+  
+  p <- c(0.11, 0.26, 0.37, 0.26)
+  theta <- c(-3.5,-1.2, 0.3, 1.7)
+  sigma <- c(0.3, 0.3, 0.3, 0.3)
+  params <- data.frame(cbind(p, theta, sigma))
+  gp=geneticParams(K=4, states=0:3, xi=c(0.3, 0.3, 0.3, 0.3), 
+                   mu=c(-3.5, -1.2, 0.3, 1.7))
+  
+  dat2 <- simulate_data_multi(params, N=500, error=0, gp)
+  expect_that(length(levels(factor(dat2$data$copy_number))) == length(theta), is_true())
+  expect_equal(p, dat2$params$p, tolerance=0.05)
+  expect_equal(theta, dat2$params$theta, tolerance=0.03)
+  expect_equal(sigma, dat2$params$sigma, tolerance=0.03)
+  
+  p <- c(0.49, 0.42, 0.09)
+  theta <- c(0.3, 1.7, 4)
+  sigma <- c(0.2, 0.2, 0.2)
+  params <- data.frame(cbind(p, theta, sigma))
+  gp=geneticParams(K=3, states=2:4, xi=c(0.2, 0.2, 0.2), 
+                   mu=c(0.3, 1.7, 4))
+  
+  dat2 <- simulate_data_multi(params, N=500, error=0, gp)
+  expect_that(length(levels(factor(dat2$data$copy_number))) == length(theta), is_true())
+  expect_equal(p, dat2$params$p, tolerance=0.05)
+  expect_equal(theta, dat2$params$theta, tolerance=0.03)
+  expect_equal(sigma, dat2$params$sigma, tolerance=0.03)
+  path <- system.file("extdata", package="marimba2")
+  saveRDS(dat2, paste0(path,"/simulation_test.rds"))
+  
+  p <- c(0.55, 0.45)
+  theta <- c(0.3, 1.7)
+  sigma <- c(0.3, 0.3)
+  params <- data.frame(cbind(p, theta, sigma))
+  gp=geneticParams(K=2, states=2:3, xi=c(0.3, 0.3), mu=c(0.3, 1.7))
+  
+  dat2 <- simulate_data_multi(params, N=500, error=0, gp)
+  expect_that(length(levels(factor(dat2$data$copy_number))) == length(theta), is_true())
+  expect_equal(p, dat2$params$p, tolerance=0.05)
+  expect_equal(theta, dat2$params$theta, tolerance=0.03)
+  expect_equal(sigma, dat2$params$sigma, tolerance=0.03)
+  
 })
